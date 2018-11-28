dd801a6d762c1c16f26f94a60a665325a35b1294
Brent Morrison <brent.morrison@hotmail.com>
2018-11-28 20:15:35 +0000

    Update comments

--- Fin_reporting.R
+++ Fin_reporting.R
@@ -1,30 +1,62 @@
-# packages
+# required packages
 library("tidyverse")
 library("DescTools")
 library("lubridate")
 
+
+#=========================================================================================
+#==    Script deriving opening to closing reconciliation by month                       ==
+#==    Data to be providing in the following format:                                    ==
+#==    - "cust"  Customer account identifier                                            ==
+#==    - "year"  Year of balance                                                        ==
+#==    - "mth"   Month of balance                                                       ==
+#==    - "unit"  Reporting unit                                                         ==
+#==    - "ccy"   Currency of the loan account                                           ==
+#==    - "type"  Type of loan, 'term' or 'rvlv', 'rvlv' are revolving loans             ==
+#==    - "stage" The IFRS9 stage (1,2, 3)                                               ==
+#==    - "poci"  Purchased or Originated Credit Impaired loan (Y / N)                   ==
+#==    - "gca"   Gross carrying amount, the loan balance as it appears in the general   ==
+#==              ledger, this may differ to the customer balance due to effective       ==
+#==              interest rate accounting of feed                                       ==
+#==    - "bal"   Customer balance of loan                                               ==
+#==    - "ecl"   Expected credit loss balance                                           ==
+#==    - "wof"   Year to date write-off balance                                         ==
+#==    - "pryr"  The 12 month ECL using the prior period methodology                    ==
+#==    - "prlt"  The lifetime ECL using the prior period methodology                    ==
+#=========================================================================================
+
+
 #=========================================================================================
 #==    Load mock data                                                                   ==
-#==    1. create date & merge ID's for distinct records                                 ==
-#==    2. remove notes  (not required for real data)                                    ==
-#==    3. expand for all combinations of month and account                              ==
-#==    4. order so group by for change works correctly                                  ==
-#==    5. replace na's created by step 3                                                ==
-#==    6. fill type attribute to newly created records                                  ==
+#==    1. read raw data                                                                 ==
+#==    2. repeat data for performance testing                                           ==
+#==    3. create date                                                                   ==
+#==    4. expand for all combinations of month and account                              ==
+#==    5. fill missing account values (those NA's after step 4)                         ==
+#==    6. join account and unit to create distinct records & avoid duplicates           ==
+#==    7. replace na's created at step 4 woth zero                                      ==
+#==    8. find minimum date                                                             ==
 #=========================================================================================
 
-bln_ctgy <- as.tibble(read.csv("data_v01.csv", header= T, sep = ",")) %>%           
-  mutate(date = as.Date(paste(year, mth, 1, sep = "-"))) %>%                            #1
-  unite(cust, cust, unit, sep = "_") %>%                     
-  select(-Notes) %>%                                                                    #2
-  complete(date = seq.Date(min(date), max(date), by = "month"), cust) %>%               #3
-  arrange(cust, date) %>%                                                               #4
-  replace_na(list(gca = 0, bal = 0, ecl = 0, wof = 0, 
-                  pryr = 0, prlt = 0))  %>%                                             #5
-  group_by(cust) %>% fill(type) %>% ungroup                                             #6
+n <- 5 # number of repeats
+x <- 6 # number of accounts in data
+data <- read.csv("data_v02.csv", header= T, sep = ",")                                  #1
+bln_ctgy<- data.frame(data,i = rep(1:n, ea = NROW(data))) %>%                           #2
+  mutate(cust.i = ((i - 1) * x) + cust) %>% select(-cust, -i) %>% 
+  rename(cust = cust.i) %>% as.tibble() %>% 
+  mutate(date = as.Date(paste(year, mth, 1, sep = "-"))) %>%                            #3
+  complete(date = seq.Date(min(date), max(date), by = "month"), cust) %>%               #4
+  arrange(cust, date) %>% group_by(cust) %>% 
+  mutate(type = coalesce(type, na.omit(type)[1]),                                       #5
+         ccy  = coalesce(ccy, na.omit(ccy)[1]), 
+         unit = coalesce(unit, na.omit(unit)[1])) %>%
+  unite(cust, cust, unit, sep = "_") %>%                                                #6                 
+  replace_na(list(gca = 0, bal = 0, ecl = 0, wof = 0,                                   #7
+                  pryr = 0, prlt = 0))  %>%
+  ungroup
 
 min.date <- bln_ctgy %>% slice(which.min(date)) %>% select(date)
-#df <- bind_rows(replicate(100, bln_ctgy, simplify = FALSE)) copy data frame
+
 
 #=========================================================================================
 #==    Create stage attributes & movement balances                                      ==
@@ -59,8 +91,8 @@ bln_ctgy <- bln_ctgy %>%
          bal.pr    = ifelse(date == min.date, bal, NA),
          ecl.op    = lag(ecl),
          ctgy.op   = lag(ctgy)) %>% fill(bal.pr) %>% 
-  rename(gca.cl    = gca, bal.cl = bal, ecl.cl = ecl, 
-         ctgy.cl   = ctgy, wof.cl = wof) %>%                                            #4
+  rename(gca.cl    = gca, bal.cl = bal, ecl.cl = ecl,                                   #4
+         ctgy.cl   = ctgy, wof.cl = wof) %>%                                            
   mutate(bal.y     = bal.cl - bal.pr + cumsum(wof.cl),                                  #5
          bal.y.dd  = if_else(bal.y > 0, bal.y, 0),
          bal.y.rd  = if_else(bal.y < 0, bal.y, 0)) %>% 
@@ -111,9 +143,16 @@ bln_mvnt <- bln_ctgy %>% group_by(cust) %>%
          ecl.m.prm    = case_when(ctgy.cl == 1 & pryr != 0 ~ ecl.cl + pryr,
                                   ctgy.cl != 1 & prlt != 0 ~ ecl.cl + prlt,
                                   TRUE ~ 0),
-         ecl.m.rem.mig= if_else(ctgy.dir != 'U', ecl.cl - ecl.op - ecl.m.dd.r - ecl.m.dd.t - ecl.m.rd.t.f - ecl.m.rd.t - ecl.m.rd.r - ecl.m.wof - ecl.m.prm, 0),
-         ecl.m.rem    = if_else(ctgy.dir == 'U', ecl.cl - ecl.op - ecl.m.dd.r - ecl.m.dd.t - ecl.m.rd.t.f - ecl.m.rd.t - ecl.m.rd.r - ecl.m.wof - ecl.m.prm, 0),
-         ecl.tfr.pre  = ecl.op + ecl.m.dd.r + ecl.m.dd.t + ecl.m.rd.t.f + ecl.m.rd.t + ecl.m.rd.r,
+         ecl.m.rem.mig= if_else(ctgy.dir != 'U', ecl.cl - ecl.op - ecl.m.dd.r - 
+                                                 ecl.m.dd.t - ecl.m.rd.t.f - 
+                                                 ecl.m.rd.t - ecl.m.rd.r - 
+                                                 ecl.m.wof - ecl.m.prm, 0),
+         ecl.m.rem    = if_else(ctgy.dir == 'U', ecl.cl - ecl.op - ecl.m.dd.r - 
+                                                 ecl.m.dd.t - ecl.m.rd.t.f - 
+                                                 ecl.m.rd.t - ecl.m.rd.r - 
+                                                 ecl.m.wof - ecl.m.prm, 0),
+         ecl.tfr.pre  = ecl.op + ecl.m.dd.r + ecl.m.dd.t + 
+                        ecl.m.rd.t.f + ecl.m.rd.t + ecl.m.rd.r,
          ecl.m.tfr.o  = -case_when(ctgy.dir != 'U' & pre.post == 'pre' ~ ecl.tfr.pre,
                                    ctgy.dir != 'U' & pre.post == 'post' ~ ecl.op,
                                    TRUE ~ 0),
@@ -121,8 +160,8 @@ bln_mvnt <- bln_ctgy %>% group_by(cust) %>%
   select(-g.tfr.pre, -ecl.tfr.pre) %>% 
   mutate_at(vars(starts_with("g.")), funs(replace_na(., 0))) %>% 
   mutate_at(vars(starts_with("i.")), funs(replace_na(., 0))) %>% 
-  mutate(gca.ch  = gca.op + rowSums(select(., contains("g."))) - gca.cl,
-         ecl.ch  = ecl.op + rowSums(select(., contains("i."))) - ecl.cl)
+  mutate(gca.ch  = gca.op + rowSums(select(., contains("gca."))) - gca.cl,
+         ecl.ch  = ecl.op + rowSums(select(., contains("ecl."))) - ecl.cl)
 
 
 #=========================================================================================
@@ -146,23 +185,21 @@ bln_mvnt_long <- bln_mvnt %>%
                           ctgy.op,
                           TRUE ~ ctgy.cl), 
          m.type = if_else(str_detect(m.type, "tfr"), 
-                        paste("tfr", ctgy.op, ctgy.cl, sep = "."), 
-                        m.type),
+                          paste("tfr", ctgy.op, ctgy.cl, sep = "."), 
+                          m.type),
          year = year(date),
          month = month(date)) %>% 
   select(date, year, month, cust, ctgy.op, ctgy.cl, ctgy, bal.type, m.type, tran_ccy) %>% 
   arrange(cust, date, bal.type)
 
-# TO DO: remove attributes not used (pre.post, ctgy.op/.cl)
-# TO DO: add GCA / ECL attribute based on gca/g. and ecl/i.
 # TO DO: unsure no duplicates over account/company/month
-# TO DO xxx <- bln_mvnt_long %>% mutate(new = if_else(str_detect(m.type, "tfr"), paste("tfr", ctgy.op, ctgy.cl, sep = "."), m.type))
 
 #=========================================================================================
 #==     Write csv                                                                       ==
 #=========================================================================================
 
 write.csv(bln_mvnt_long, file = "bln_mvnt_long.csv")
+saveRDS(bln_mvnt_long, file="bln_mvnt_long.Rda")
 
 
 
@@ -181,3 +218,5 @@ op_jan <- bln_mvnt_long %>% filter(m.ment == "gca.op" & date == as.Date('2018-03
 dec_jan<- full_join(cl_dec, op_jan, by = "cust") %>% 
   mutate(check = tran_ccy1.x - tran_ccy1.y) %>% 
   filter(check != 0)
+
+
